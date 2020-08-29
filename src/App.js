e42073a7691ca90c32bbbb8dbf03a48cc361d5bf
Liz Aitken <liz.a.aitken@gmail.com>
2020-08-29 18:15:15 -0400

    Added alphabet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import Board from './components/Board';
+import Card from './components/Card';
+
 
 function App() {
+  let al = ("abcdefghijklmnopqrstuvwxyz").split("");
+  console.log(al);
+
   return (
     <div className="App">
       <h1>Alphabet Game</h1>
+      <main className= "flexbox">
+          <Board id="board-1" className="board">
+            { al.map((letter, i) => {
+                 return <Card key={i} id={`card-${i}`} className="card" draggable="true">
+                  <p>{letter}</p>
+                </Card>
+              })
+              
+            }
+          </Board>
+
+          <Board id="board-2" className="board">
+            
+          </Board>
+      </main>
     </div>
   );
 }
